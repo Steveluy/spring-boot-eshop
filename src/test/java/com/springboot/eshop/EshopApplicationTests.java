1c3ef9f5fe58c8f21787db845b04d5721248ef83
stevelu <stevelu123@163.com>
2019-12-21 13:38:37 +0800

    druid

--- src/test/java/com/springboot/eshop/EshopApplicationTests.java
+++ src/test/java/com/springboot/eshop/EshopApplicationTests.java
@@ -1,13 +1,25 @@
 package com.springboot.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.print(dataSource.getClass());
+        Connection connection=dataSource.getConnection();
+        System.out.print(connection);
+        connection.close();
+
     }
 
 }

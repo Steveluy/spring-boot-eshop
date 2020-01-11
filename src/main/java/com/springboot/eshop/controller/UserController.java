ea15facb851c8919a639121237008f1fe5d7b837
stevelu <stevelu123@163.com>
2020-01-11 11:06:07 +0800

    添加mybatis pagehelper支持

--- src/main/java/com/springboot/eshop/controller/UserController.java
+++ src/main/java/com/springboot/eshop/controller/UserController.java
@@ -3,25 +3,23 @@ package com.springboot.eshop.controller;
 import com.springboot.eshop.entity.usersInfo;
 import com.springboot.eshop.service.usersInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value ="/user")
 public class UserController {
     @Autowired
     private usersInfoService usersInfo;
 
-    @ResponseBody
-    @RequestMapping(value = "/insert", produces = {"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/insert", method= RequestMethod.POST)
     public int insert(usersInfo users){
         return usersInfo.addUser(users);
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/all/{pageNum}/{pageSize}", method= RequestMethod.GET)
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
 
         return usersInfo.selectAllUser(pageNum,pageSize);
